81f453277f3c35d9cd80cfc536f609058f9f1931
livaan <vanlinh30122003@gmail.com>
2025-03-21 00:34:49 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home.js";
 import Search from "./pages/Search.js";
